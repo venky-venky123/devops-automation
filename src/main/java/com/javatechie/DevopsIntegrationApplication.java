ec3d52ef6cd987f08dea4e05b06a61b19e94d9cc
rahulchinnam <rahulchinnam1502@gmail.com>
2025-04-06 12:36:21 +0530

    modified

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return " Hello Rahul,You're Welcome... ";
+		return " index";
 	}
 
 	public static void main(String[] args) {
